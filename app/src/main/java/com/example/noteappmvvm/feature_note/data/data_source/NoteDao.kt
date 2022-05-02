83b1505c91751b3bf616c01313f2e81ea60d4884
TanjotP <tanjotspanesar@gmail.com>
2022-05-02 19:35:28 -0400

    initial commit, viewmodel, dao, usecases

---
+++ app/src/main/java/com/example/noteappmvvm/feature_note/data/data_source/NoteDao.kt
@@ -0,0 +1,24 @@
+package com.example.noteappmvvm.feature_note.data.data_source
+
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.noteappmvvm.feature_note.domain.model.Note
+import kotlinx.coroutines.flow.Flow
+
+interface NoteDao {
+
+    //not suspend function because returns a flow.
+    @Query("SELECT * FROM note")
+    fun getNotes(): Flow<List<Note>>
+
+    @Query("SELECT * FROM note WHERE id = :id")
+    suspend fun getNoteById(id: Int): Note?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNote(note: Note)
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+}
---
+++ app/src/main/java/com/example/noteappmvvm/feature_note/domain/use_case/NoteUseCases.kt
@@ -0,0 +1,9 @@
+package com.example.noteappmvvm.feature_note.domain.use_case
+
+class NoteUseCases(
+    val getNotesUseCase: GetNotesUseCase,
+    val deleteNoteUseCase: DeleteNoteUseCase,
+    val addNoteUseCase: AddNoteUseCase
+) {
+
+}
